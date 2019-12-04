e1e73782b2d92e9abba552e346bdd1567b4414f8
Luke4211 <34666766+Luke4211@users.noreply.github.com>
2019-12-04 00:16:55 -0500

    controller

--- P2PVoice/src/Controller.java
+++ P2PVoice/src/Controller.java
@@ -1,5 +1,8 @@
 public class Controller {
-    MainFrame mf = new MainFrame();
-    Client client = new Client(mf.getPort(), mf);
-    mf.setClient(client);
+
+    public Controller() {
+        MainFrame mf = new MainFrame();
+        Client client = new Client(mf.getPort(), mf);
+        mf.setClient(client);
+    }
 }
